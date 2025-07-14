cc008017400a659bd06fbc73f70337f1a9d06709
VeeV <vahab.afsharian94@gmail.com>
2025-07-14 19:00:57 +0300

    Add react-weather-app project

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      semi: ["error", "always"],
+      "no-console": "warn",
+      // 🚨 THIS disables the React-in-scope error
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
