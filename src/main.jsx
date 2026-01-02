d91319ff0fee42770272c2b85453b2b498d4096d
Vibo <vahab.afsharian94@gmail.com>
2026-01-02 16:22:02 +0200

    feat: add @vercel/analytics for enhanced analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/next";
 import App from "./App.jsx";
+import { StrictMode } from "react";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+    <Analytics />
+  </StrictMode>,
+);
